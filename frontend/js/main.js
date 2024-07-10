5ac00c295d887e9ada4433eeba10580b98d26c4f
d.koenig <s1077095@stud.sbg.ac.at>
2024-07-10 22:21:07 +0200

    changed navbar for homepage

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
             title: 'Strondbodbuam',
             description: 'Ein innovativer Counter für Hallstättersee-Sprünge mit kreativen CSS-Animationen.',
             image: '/projects/strondbodbuam/see.webp',
-            link: '/projects/strondbodbuam/index.html',
+            link: '/strondbodbuam',
             passwordProtected: true
         },
         {
